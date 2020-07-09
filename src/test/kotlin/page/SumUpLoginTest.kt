091c6333118c19696f7c8f5286d9c0c408437bca
Tony Hadjiivanov <tony.hadjiivanov@gmail.com>
2020-07-09 23:08:30 +0300

    Ensure the Sales page is loaded before looking for Transaction history

--- src/test/kotlin/page/SumUpLoginTest.kt
+++ src/test/kotlin/page/SumUpLoginTest.kt
@@ -2,8 +2,8 @@ package page
 
 import base.TestBase
 import org.testng.annotations.Test
-import java.lang.Exception
 import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class SumUpLoginTest : TestBase() {
 
@@ -14,6 +14,7 @@ class SumUpLoginTest : TestBase() {
         val loginPage = homePage.goToLoginPage()
         val accountPage = loginPage.login("tony.hadjiivanov@gmail.com", "%9aA&llTnQ\$t7hn9")
         val salesPage = accountPage.goToSalesPage()
+        assertTrue(salesPage.transactionSearchBoxIsDisplayed(), "The sales page was not opened.")
         assertFalse(salesPage.transactionListIsDisplayed(), "An incomplete account can not have a transaction history.")
     }
 }
