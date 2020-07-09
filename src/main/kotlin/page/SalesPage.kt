800f317a738bdbd63d259e9991392e187f8e40df
Tony Hadjiivanov <tony.hadjiivanov@gmail.com>
2020-07-09 23:05:15 +0300

    Add function to ensure that Sales page is loaded

--- src/main/kotlin/page/SalesPage.kt
+++ src/main/kotlin/page/SalesPage.kt
@@ -12,11 +12,18 @@ class SalesPage(private val driver: WebDriver) : BasePage(driver) {
     @FindBy(xpath = "//*[@data-selector='TRANSACTIONS_HISTORY.TRANSACTION_LIST']")
     private val transactionList: WebElement? = null
 
-    @FindBy(xpath = "//*[@data-selector='CALENDAR_BUTTON']")
-    private val calendarButton: WebElement? = null
+    @FindBy(xpath = "//*[@data-selector='TRANSACTIONS_FILTERS.SEARCH']")
+    private val transactionSearchBox: WebElement? = null
+
+    fun transactionSearchBoxIsDisplayed(): Boolean {
+        try {
+            transactionSearchBox?.isDisplayed?.let { return it } ?: return false
+        } catch (e: org.openqa.selenium.NoSuchElementException) {
+            return false
+        }
+    }
 
     fun transactionListIsDisplayed(): Boolean {
-        WebDriverWait(driver, 5).until(ExpectedConditions.elementToBeClickable(calendarButton))
         try {
             transactionList?.isDisplayed?.let { return it } ?: return false
         } catch (e: org.openqa.selenium.NoSuchElementException) {
