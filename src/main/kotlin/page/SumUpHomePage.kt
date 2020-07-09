6a272fc2521c2c4f11a2c720b1cbc8b8b2c3fd04
Tony Hadjiivanov <tony.hadjiivanov@gmail.com>
2020-07-09 23:07:11 +0300

    Explicitly wait for the login button to become clickable

--- src/main/kotlin/page/SumUpHomePage.kt
+++ src/main/kotlin/page/SumUpHomePage.kt
@@ -4,6 +4,8 @@ import base.BasePage
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.WebElement
 import org.openqa.selenium.support.FindBy
+import org.openqa.selenium.support.ui.ExpectedConditions
+import org.openqa.selenium.support.ui.WebDriverWait
 
 class SumUpHomePage(private val driver: WebDriver) : BasePage(driver) {
 
@@ -11,6 +13,7 @@ class SumUpHomePage(private val driver: WebDriver) : BasePage(driver) {
     private val loginButton: WebElement? = null
 
     fun goToLoginPage(): LoginPage {
+        WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(loginButton))
         loginButton?.click()
         return LoginPage(driver)
     }
